b929146950b1953c9c47aca168aabba656c74205
Jørgen Christian Heinsen <jhe@trifork.com>
2023-06-14 11:38:45 +0200

    Added UnitTests workflow.

--- kotlin-data-wedge-lib/build.gradle.kts
+++ kotlin-data-wedge-lib/build.gradle.kts
@@ -25,11 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     testOptions {
         unitTests {
@@ -37,6 +34,11 @@ android {
         }
     }
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
 
 dependencies {
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
+}
+
 rootProject.name = "kotlin-data-wedge"
 include(":kotlin-data-wedge-lib")
